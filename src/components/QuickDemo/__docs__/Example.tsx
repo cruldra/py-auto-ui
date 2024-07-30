9cf8103366361e38a8aa563a3fa776a4c0bb58ef
cruldra <cruldra@gmail.com>
2024-07-31 01:25:16 +0800

    初始化
    #time 17m

---
+++ src/components/QuickDemo/__docs__/Example.tsx
@@ -0,0 +1,19 @@
+import {FC} from "react";
+import QuickDemo from "../QuickDemo";
+
+const Example: FC = ({}) => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+            }}
+        >
+            <QuickDemo/>
+        </div>
+    );
+};
+
+export default Example;
